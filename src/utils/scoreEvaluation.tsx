5f4559f315f4b96bcb8cf183cb391e74df5da670
Max Bengtson <maxbengtson90@gmail.com>
2023-11-08 10:27:46 +0100

    feat(scoreevaluation):implemented the elo rating in the scoreevaluation

--- src/utils/scoreEvaluation.tsx
+++ src/utils/scoreEvaluation.tsx
@@ -4,27 +4,66 @@ import Playerinfo from "@components/molecules/Playerinfo";
 import { GameContextType } from "providers/GameProvider";
 import useHandleGameSession from "@hooks/useGetPlayers/useHandleGameSession";
 import { updatePlayerStats } from "@firebase/firebase";
+
+import { calculatePlayerRatings } from "@utils/eloRating";
 import { PlayerInGame } from "@types/Game.types";
 
 const scoreEvaluation = (player1: PlayerInGame, player2: PlayerInGame) => {
-  if (player1.points > player2.points) {
-    console.log(`${player1.name} wins! ${player1.wins} <---- Score evaluation`);
-    console.log(
-      `${player2.name} loses! ${player2.losses} <---- Score evaluation`
-    );
+  const playerOneWins = player1.points > player2.points;
+  const playerTwoWins = player1.points < player2.points;
+  const evenScore = player1.points === player2.points;
+
+  if (playerOneWins) {
     player1.wins += 1;
     player2.losses += 1;
-    updatePlayerStats(player1.id, player1.wins, player1.losses);
-    updatePlayerStats(player2.id, player2.wins, player2.losses);
-  } else if (player1.points < player2.points) {
-    console.log(`${player2.name} ${player2.wins} wins! <---- Score evaluation`);
-    console.log(
-      `${player1.name} ${player1.losses} losses! <---- Score evaluation`
-    );
+
+    console.log("Player one won");
+    const calculatedRating = calculatePlayerRatings([
+      { ...player1, playerWon: true },
+      { ...player2, playerWon: false },
+    ]);
+    console.log(calculatedRating, "<-----");
+
+    updatePlayerStats({
+      playerId: player1.id,
+      wins: player1.wins,
+      losses: player1.losses,
+      rating: calculatedRating.newRatingOne,
+    });
+
+    updatePlayerStats({
+      playerId: player2.id,
+      wins: player2.wins,
+      losses: player2.losses,
+      rating: calculatedRating.newRatingTwo,
+    });
+
+    return calculatedRating;
+  } else if (playerTwoWins) {
     player1.losses += 1;
     player2.wins += 1;
-    updatePlayerStats(player1.id, player1.wins, player1.losses);
-    updatePlayerStats(player2.id, player2.wins, player2.losses);
+    console.log("Player two won");
+    const calculatedRating = calculatePlayerRatings([
+      { ...player1, playerWon: false },
+      { ...player2, playerWon: true },
+    ]);
+    updatePlayerStats({
+      playerId: player1.id,
+      wins: player1.wins,
+      losses: player1.losses,
+      rating: calculatedRating.newRatingOne,
+    });
+
+    updatePlayerStats({
+      playerId: player2.id,
+      wins: player2.wins,
+      losses: player2.losses,
+      rating: calculatedRating.newRatingTwo,
+    });
+    return calculatedRating;
+  } else {
+    //TODO - Skicka något som gör att det inte går att avsluta
+    return evenScore;
   }
 };
 export default scoreEvaluation;
