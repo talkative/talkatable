2132d7d0265e4e0f24cdb486b2728c377aed876a
Max Bengtson <maxbengtson90@gmail.com>
2023-11-13 15:45:50 +0100

    refactor(score-evalutation): ratio-counting added and ratio-field added to player two

--- src/utils/scoreEvaluation.tsx
+++ src/utils/scoreEvaluation.tsx
@@ -1,31 +1,27 @@
-import { useGameContext } from "providers/GameProvider";
-import { useState } from "react";
-import Playerinfo from "@components/molecules/Playerinfo";
-import { GameContextType } from "providers/GameProvider";
-import useHandleGameSession from "@hooks/useGetPlayers/useHandleGameSession";
 import { updatePlayerStats } from "@firebase/firebase";
 
 import { calculatePlayerRatings } from "@utils/eloRating";
 import { PlayerInGame } from "@types/Game.types";
 import { getRank } from "./ranks";
 
+//TODO Fixa till koden, gör det mindre och bättre
 const scoreEvaluation = (player1: PlayerInGame, player2: PlayerInGame) => {
   const playerOneWins = player1.points > player2.points;
   const playerTwoWins = player1.points < player2.points;
-  const evenScore = player1.points === player2.points;
-  const playerOneRatio = player1.wins / player1.losses;
-  const playerTwoRatio = player2.wins / player2.losses;
+  const playerOneRatio =
+    player1.losses !== 0 ? player1.wins / player1.losses : player1.wins;
+  const playerTwoRatio =
+    player2.losses !== 0 ? player2.wins / player2.losses : player2.wins;
 
   if (playerOneWins) {
     player1.wins += 1;
     player2.losses += 1;
 
-    console.log("Player one won");
     const calculatedRating = calculatePlayerRatings([
       { ...player1, playerWon: true },
       { ...player2, playerWon: false },
     ]);
-    console.log(calculatedRating, "<-----");
+
     const newRankPlayerOne = getRank(calculatedRating.newRatingOne);
     const newRankPlayerTwo = getRank(calculatedRating.newRatingTwo);
 
@@ -46,13 +42,12 @@ const scoreEvaluation = (player1: PlayerInGame, player2: PlayerInGame) => {
       ratio: playerTwoRatio,
       rank: newRankPlayerTwo,
     });
-    console.log(playerOneRatio, "<--- Player one ratio");
-    console.log(playerTwoRatio, "<---- Player two ratio");
+
     return calculatedRating;
   } else if (playerTwoWins) {
     player1.losses += 1;
     player2.wins += 1;
-    console.log("Player two won");
+
     const calculatedRating = calculatePlayerRatings([
       { ...player1, playerWon: false },
       { ...player2, playerWon: true },
@@ -63,6 +58,7 @@ const scoreEvaluation = (player1: PlayerInGame, player2: PlayerInGame) => {
       playerId: player1.id,
       wins: player1.wins,
       losses: player1.losses,
+      ratio: playerOneRatio,
       rating: calculatedRating.newRatingOne,
       rank: newRankPlayerOne,
     });
@@ -71,13 +67,12 @@ const scoreEvaluation = (player1: PlayerInGame, player2: PlayerInGame) => {
       playerId: player2.id,
       wins: player2.wins,
       losses: player2.losses,
+      ratio: playerTwoRatio,
       rating: calculatedRating.newRatingTwo,
       rank: newRankPlayerTwo,
     });
     return calculatedRating;
   } else {
-    //TODO - Skicka något som gör att det inte går att avsluta
-    return evenScore;
   }
 };
 export default scoreEvaluation;
