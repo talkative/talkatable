cc9a02239c03932c5ea0a8746cadada57b8d7cb6
Max Bengtson <maxbengtson90@gmail.com>
2023-11-13 16:20:52 +0100

    feat(navigation): created a backbutton as atom

---
+++ src/components/atoms/BackButton/BackButton.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { HtmlHTMLAttributes } from "react";
+import cn from "@utils/classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+interface BackButton extends HtmlHTMLAttributes<HTMLDivElement> {
+  onClick: () => void;
+}
+
+export const BackButton = ({ onClick, ...props }: BackButton) => {
+  return (
+    <div className={cn("relative")} {...props}>
+      <button
+        onClick={onClick}
+        className={cn(
+          "rounded-full bg-button-color text-white w-20 h-20 top-4 left-4 shadow-xl"
+        )}
+      >
+        <FontAwesomeIcon
+          icon={faArrowRight}
+          rotation={180}
+          size="2xl"
+          style={{ color: "#FFFF" }}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default BackButton;
---
+++ src/components/atoms/BackButton/index.ts
@@ -0,0 +1,5 @@
+import BackButton from "./BackButton";
+
+export default BackButton;
+
+export * from "./BackButton";
