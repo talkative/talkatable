4db7e17403eeeaaa22e47e8311e669ed4bc78687
Max Bengtson <max@talkative.se>
2023-11-28 13:44:42 +0100

    fix(logo): trying to commit to fix the talkative-logo on the homescreen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,9 +16,9 @@ const Home = () => {
     <div className="w-screen h-screen bg-background-color flex flex-col sm:h-screen">
       <div className="flex justify-center items-center h-3/4 overflow-hidden p-4">
         <img
-          src="public/Talkative-logo.png"
-          alt="Talkative-logo.png"
-          className="w-2/3 animate-spin-slow "
+          src="/Talkative-logo.png"
+          alt="Talkative-logo"
+          className="w-2/3 animate-spin-slow"
         />
       </div>
       <div className="p-4">
